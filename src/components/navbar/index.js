7d645855bf72e1e35baf168ae9b1cf0e6dabc0d5
Khaled Maher <maxios.code@gmail.com>
2020-08-01 16:08:50 +0200

    add navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import Link from 'gatsby-plugin-transition-link'
 
 const Navbar = () => (
-  <div> hello world navbar </div>
+  <div className="navbar__container font-white font__RC-light">
+    <ul className="d-flex">
+      <Link to="/blog">
+        <li className="navbar__item">Blog</li>
+      </Link>
+      <li className="navbar__item">Pockets</li>
+      <li className="navbar__item">Portfolio</li>
+      <li className="navbar__item">Resume</li>
+    </ul>
+  </div>
 )
 
 export default Navbar;
--- src/pages/intro.js
+++ src/pages/intro.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Link from 'gatsby-plugin-transition-link';
 
+import Navbar from '@components/navbar';
 import Layout from '@components/layout';
 import Photo from '@components/photo';
 import SVG from '@components/svg';
@@ -11,44 +12,46 @@ import GithubIcon from '@images/icons/github.svg';
 const IntroPage = () => {
   return (
     <Layout>
-      <div className="intro__header">
+      <div className="intro__header d-flex justify-content-center align-items-end">
+        <Navbar/>
       </div>
       <div className="intro__content d-flex justify-content-sm-center">
-        <div className="intro__photo-container d-flex flex-column justify-content-center">
 
+        <div className="intro__photo-container d-flex flex-column justify-content-center">
           {/* profile picture */}
           <div className="d-flex justify-content-center">
             <Photo className="intro__photo"/>
           </div>
+          {/* profile social */}
           <div className="mt-2 intro__social d-flex justify-content-center">
             <div className="ml-4 mr-4 icon pointer">
               <a target="_blank" rel="noreferrer" href="https://twitter.com/animaxios">
-                <SVG id={TwitterIcon.id} size={50} color="#272E40"/>
+                <SVG id={TwitterIcon.id} size={30} color="#272E40"/>
               </a>
             </div>
 
             <div className="ml-4 mr-4 icon pointer">
               <a target="_blank" rel="noreferrer" href="https://linkedin.com/in/khaledmaher">
-                <SVG id={LinkedinIcon.id} size={50} color="#272E40"/>
+                <SVG id={LinkedinIcon.id} size={30} color="#272E40"/>
               </a>
             </div>
 
             <div className="ml-4 mr-4 icon pointer">
               <a target="_blank" rel="noreferrer" href="https://github.com/maxios">
-                <SVG id={GithubIcon.id} size={50} color="#272E40"/>
+                <SVG id={GithubIcon.id} size={30} color="#272E40"/>
               </a>
             </div>
-          </div>
+          </div> {/* profile social */}
         </div>
 
         {/* content message */}
         <div className="container row">
           <section className="intro__message col-md-8 col-sm-12">
             <h1 className="font__title--big">
-              Hi there!, I’m <span className="font__R-medium">Khaled Maher</span>
+              Hi there!, I’m <span className="font__R-medium">Khaled</span>
             </h1>
 
-            <p className="pt-5 pr-3 font__message">
+            <p className="pt-5 pr-md-3 font__message">
               A young software engineering fellow. love to discover new patterns in life in legacy thoughts in order to freshen the discussions.
               Happy to share my thoughts at my personal <Link to="/blog">blogs</Link>. check it out.
 
@@ -58,6 +61,11 @@ const IntroPage = () => {
 
           <section className="mt-5 intro__posts col-md-4 col-sm-12">
             <h2 className="font__title mb-5"> Recent Posts </h2>
+
+            <div className="intro__post">
+              <h3 className="font__title">The mental models of the thoughts</h3>
+              <p className="font__body">20 July, 2020 - 08:00 AM</p>
+            </div>
 
             <h3 className="font__title">The mental models of the thoughts</h3>
             <p className="font__body">20 July, 2020 - 08:00 AM</p>
