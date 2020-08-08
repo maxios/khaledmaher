1936f801430294f9b8b355eac668a180c79c9632
Khaled Maher <maxios.code@gmail.com>
2020-08-09 01:09:57 +0200

    fix webstie seo component

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -3,15 +3,18 @@ import PropType from 'prop-types';
 import Footer from '@components/footer';
 import SEO from "@components/seo"
 
-const Layout = props => (
-  <div className="layout__container">
-    <SEO title="Khaled Maher | Software Engineer" />
-    {props.children}
-    <Footer/>
-  </div>
-)
+const Layout = props => {
+  return (
+    <div className="layout__container">
+      <SEO {...props.seo} />
+      {props.children}
+      <Footer/>
+    </div>
+  )
+}
 
 Layout.propTypes = {
+  seo: PropType.object.isRequired,
   children: PropType.any.isRequired
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from 'gatsby-plugin-transition-link';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-svg-core/styles.css'
@@ -15,9 +16,14 @@ library.add(faBars);
 
 const IndexPage = () => {
 
+  const description = `
+  Dedicated Software Engineer experienced in crafting software products using various stacks alongside with AWS Services.
+  I assure the best performance, Code Readability, and fast delivery.
+  `
+
   return (
     <div className="background font-white">
-      <Layout>
+      <Layout seo={{title: 'Khaled Maher | Software Engineer', description, image: Image}}>
         <div className="home__container">
           <div className="home__photo">
             <Photo image={Image} width={180}/>
@@ -26,8 +32,8 @@ const IndexPage = () => {
             <h1 className="greetings font-white text-centered"> Hi </h1>
             <br/>
             <p className="p-4 font__message">
-              Dedicated Software Engineer experienced in crafting software products using various stacks alongside with AWS Services.
-              <br/>I assure the best performance, Code Readability, and fast delivery.
+              {description}
+              <br/> happy to <Link className="font__link" to="/blog"> check my Blog </Link>
             </p>
 
             <br/>
