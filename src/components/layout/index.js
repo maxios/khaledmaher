4cdcc21f4fd8ab1892d6469f88f4c1dcc511ddb5
Khaled Maher <maxios.code@gmail.com>
2020-08-27 14:21:18 +0200

    fix chrome mobile crop the bottom

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -4,21 +4,24 @@ import { Helmet } from 'react-helmet';
 import NavBar from '@components/navbar';
 import Footer from '@components/footer';
 import SEO from "@components/seo"
+import Div100vh from 'react-div-100vh'
 
 const Layout = props => {
   return (
-    <div className="layout__container">
-      {!props.hideNavbar && <NavBar/> }
-      <Helmet>
-        <style>{`body { background-color: ${props.bg}; }`}</style>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
-      </Helmet>
-      <SEO {...props.seo} />
-      {props.children}
-      <div className="layout__footer mb-5">
-        <Footer/>
+    <Div100vh>
+      <div className="layout__container">
+        {!props.hideNavbar && <NavBar/> }
+        <Helmet>
+          <style>{`body { background-color: ${props.bg}; }`}</style>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+        </Helmet>
+        <SEO {...props.seo} />
+        {props.children}
+        <div className="layout__footer mb-5">
+          <Footer/>
+        </div>
       </div>
-    </div>
+    </Div100vh>
   )
 }
 
