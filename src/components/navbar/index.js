ec404557957e0c1107ef984e9790e95ab541e5d4
Khaled Maher <maxios.code@gmail.com>
2020-08-28 13:29:27 +0200

    transition navbar links with fade

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import Link from 'gatsby-plugin-transition-link'
+import AniLink from 'gatsby-plugin-transition-link/AniLink'
+import { Link } from 'gatsby';
 
 const Navbar = () => (
-  <div className="navbar__container font-white font__RC-light">
-    <ul className="d-flex">
-      <Link to="/">
+  <div className="navbar__container font__RC-light">
+    <ul className="navbar__list d-flex">
+      <Link fade to="/">
         <li className="navbar__item">Index</li>
       </Link>
-      <Link to="/blog">
+      <AniLink fade to="/blog">
         <li className="navbar__item">Blog</li>
-      </Link>
-      <a href="https://drive.google.com/file/d/1JXbOtY8dl08_K39EfjjlXJ4Ou9IL-MxZ/view?usp=sharing" target="_blank">
+      </AniLink>
+      <a href="https://drive.google.com/file/d/1JXbOtY8dl08_K39EfjjlXJ4Ou9IL-MxZ/view?usp=sharing" target="_blank" rel="noreferrer">
         <li className="navbar__item">Resume</li>
       </a>
     </ul>
